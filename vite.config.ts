fab301a802c00ade638f990b19a1a389d7da80a7
jessicaagren <jessicaagren@hotmail.com>
2024-12-13 11:15:41 +0100

    vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,8 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
+    base: './', // Gör så att alla resurser använder relativa länkar
+
   build: {
     rollupOptions: {
       input: {
